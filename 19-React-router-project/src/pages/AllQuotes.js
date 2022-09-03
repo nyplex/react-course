487e8dca0ff4d3284849958431905600b054fb10
Alex Nypels <nypels.alexandre@outlook.com>
2022-09-03 10:19:28 +0100

    Adding query params

--- 19-React-router-project/src/pages/AllQuotes.js
+++ 19-React-router-project/src/pages/AllQuotes.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import QuoteList from "../components/quotes/QuoteList";
+import { getAllQuotes } from "../lib/api";
+import useHttp from "../hooks/use-http";
+
+const DUMMY_QUOTES = [
+    {
+        id: "q1",
+        author: "Alex",
+        text: "Learning React is fun",
+    },
+    {
+        id: "q2",
+        author: "Max",
+        text: "Learning React is great",
+    },
+];
 
 const AllQuotes = () => {
-  return (
-    <h1>All quotes page</h1>
-  )
-}
+  useEffect(() => {
+    sendRequest()
+  }, [])
+    const {
+        sendRequest,
+        status,
+        data: loadedQuotes,
+    } = useHttp(getAllQuotes, true);
 
-export default AllQuotes
+    return <QuoteList quotes={DUMMY_QUOTES} />;
+};
+
+export default AllQuotes;
--- 19-React-router-project/src/pages/QuoteDetail.js
+++ 19-React-router-project/src/pages/QuoteDetail.js
@@ -1,16 +1,39 @@
 import React, { Fragment } from "react";
-import { Route, useParams } from "react-router-dom";
+import { Route, useParams, Link, useRouteMatch } from "react-router-dom";
 import CommentItem from "../components/comments/CommentItem";
 import Comments from "../components/comments/Comments";
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
+
+const DUMMY_QUOTES = [
+    {
+        id: "q1",
+        author: "Alex",
+        text: "Learning React is fun",
+    },
+    {
+        id: "q2",
+        author: "Max",
+        text: "Learning React is great",
+    },
+];
 
 const QuoteDetail = () => {
     const params = useParams();
+    const match = useRouteMatch();
+    const quote = DUMMY_QUOTES.find(quote => quote.id === params.quoteId)
+    if(!quote) {
+        return <p>No quote found</p>
+    }
     return (
         <Fragment>
-            <h1>Quote detail page</h1>
-            <p>{params.quoteId}</p>
-            <Route path={`/quotes/${params.quoteId}/comments`}>
-                <Comments/>
+            <HighlightedQuote text={quote.text} author={quote.author}/>
+            <Route path={match.path} exact>
+                <div className="centered">
+                    <Link className="btn--flat" to={`${match.url}/comments`}>Load Comments</Link>
+                </div>
+            </Route>
+            <Route path={`${match.path}/comments`}>
+                <Comments />
             </Route>
         </Fragment>
     );
